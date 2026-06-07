de1d0a74d4e26ff0ab8dd40f8af818b65c9bff4f
Arushi Srivastava <srivastava.arushi0612@gmail.com>
2026-06-07 21:52:51 +0530

    Fix Railway deployment

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,11 @@
 from flask import Flask
 from flask_cors import CORS
-
-# Import blueprint
 from routes.tasks import tasks_bp
+import os
 
 app = Flask(__name__)
 CORS(app)
 
-# Register blueprint
 app.register_blueprint(tasks_bp)
 
 @app.route("/")
@@ -15,5 +13,5 @@ def home():
     return {"message": "API Running"}
 
 if __name__ == "__main__":
-    app.run(debug=True)
-
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
